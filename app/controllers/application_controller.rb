7cdf57ed79d5365d311469d87c2952d3da0ae6f2
Adam La Rosa <adam.larosa@outlook.com>
2022-04-09 18:13:17 -0700

    Locked down controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+
+    before_action :authorized
+
+    def current_user
+        # User.find_by( id: session[:user_id] )
+        true
+    end
+
+    private
+
+    def logged_in?
+        !!current_user
+    end
+
+    def authorized
+        render json: {}, status: :unauthorized unless logged_in?
+    end
 end

215071384098f733424979561476fc4785872925
Adam La Rosa <adam.larosa@outlook.com>
2022-04-09 17:43:47 -0700

    Post instance method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   belongs_to :parent, class_name: "Post", optional: true
   has_many :replies, class_name: "Post", foreign_key: "parent_id", 
     dependent: :destroy
+
+  def is_op?
+    !self.parent_id
+  end
+  
 end
